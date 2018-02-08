3ff64e9c2afa55730de5cecba1e479959d62038d
lxk <2664276518@qq.com>
2018-02-08 17:55:06 +0800

    fc

---
+++ tasks/browser.js
@@ -0,0 +1,11 @@
+import gulp from 'gulp'
+import gulpif from 'gulp-if'
+import util from 'gulp-util'
+import args from './util/args'
+
+gulp.task('browser', (cb) => {
+  if (!args.watch) returncb()
+  gulp.watch('app/**/*.js', ['scripts'])
+  gulp.watch('app/**/*.ejs', ['pages'])
+  gulp.watch('app/**/*.css', ['css'])
+})
---
+++ tasks/util/args.js
@@ -0,0 +1,38 @@
+/**
+ * 配置命令行模块
+ * 选项参数：类型、默认值、描述
+ * .argv：对输入的命令行以字符串形式解析
+ */
+import yargs from 'yargs'
+
+const args = yargs
+
+    .option('production', {
+        boolean: true,
+        default: false,
+        describe: 'min all scripts'
+    })
+
+    .option('watch', {
+        boolean: true,
+        default: false,
+        describe: 'watch all files'
+    })
+
+    .option('verbose', {
+        boolean: true,
+        default: false,
+        describe: 'log'
+    })
+
+    .option('sourcemaps', {
+        describe: 'force the creation of sourcemaps'
+    })
+
+    .option('port', {
+        string: true,
+        default: 8080,
+        describe: 'server port'
+    })
+
+    .argv
