b29a3ca6cb93637b427b67a06b639a18144d4a15
lxk <2664276518@qq.com>
2018-03-29 22:03:00 +0800

    add dev

--- tasks/css.js
+++ tasks/css.js
@@ -4,7 +4,7 @@ import livereload from 'gulp-livereload'
 import args from './util/args'
 
 gulp.task('css', () => {
-  return gulp.src('app/**/*.css')
+  return gulp.src('app/**/*.css') // 这种形式传递的文件包含css文件夹，也就是所有的*号部分都会传递
     .pipe(gulp.dest('server/public'))
     // .pipe(gulpif(args.watch, livereload()))
 })
